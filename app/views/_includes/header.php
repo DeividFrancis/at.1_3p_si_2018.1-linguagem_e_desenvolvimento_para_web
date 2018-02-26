a338eaf69d7ea9d0be58bf073ad6bd3b830e4c0b
DeividFrancis <deividfrancisdeoliveira@gmail.com>
2018-02-25 22:08:18 -0300

    add o framework materialize com link externo

--- app/views/_includes/header.php
+++ app/views/_includes/header.php
@@ -5,11 +5,11 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   </head>
   <body>
     <div class="container">
-
-    </div>
--- app/views/calculo/jurosSimplesComposto.template.php
+++ app/views/calculo/jurosSimplesComposto.template.php
@@ -1,5 +1,19 @@
 <?php $title = 'Juros Simples e Composto'; require_once '../_includes/header.php'; ?>
-
+<div class="row">
+        <div class="col s12 m6">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">Card Title</span>
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+        </div>
+      </div>
 <h5>Juros simples</h5>
 <form class="" action="../../controller/jurosSimples.controller.php" method="POST">
   capital<input type="text" name="capital">

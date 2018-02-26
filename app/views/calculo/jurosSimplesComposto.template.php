de39f89f0971ff07e70b18f411c772477bcf3697
DeividFrancis <deividfrancisdeoliveira@gmail.com>
2018-02-25 23:10:10 -0300

    Add card no layout da pagina juros simples e composto

--- app/views/calculo/jurosSimplesComposto.template.php
+++ app/views/calculo/jurosSimplesComposto.template.php
@@ -1,20 +1,63 @@
 <?php $title = 'Juros Simples e Composto'; require_once '../_includes/header.php'; ?>
-<div class="row">
-        <div class="col s12 m6">
-          <div class="card blue-grey darken-1">
-            <div class="card-content white-text">
-              <span class="card-title">Card Title</span>
-              <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
-            </div>
-            <div class="card-action">
-              <a href="#">This is a link</a>
-              <a href="#">This is a link</a>
-            </div>
-          </div>
-        </div>
-      </div>
-<h5>Juros simples</h5>
+<div class="card">
+<div class="card-content">
+  <div class="card-title">Juros Simples</div>
+<form class="col s12" action="../../controller/jurosSimples.controller.php" method="POST">
+  <div class="row">
+    <div class="input-field col s6">
+      <input id="capital" type="text" name="capital">
+      <label for="capital">Capital (R$)</label>
+    </div>
+    <div class="input-field col s6">
+      <input id="taxa" type="text" name="taxa" class="validate">
+      <label for="taxa">Taxa (% ao Mês)</label>
+    </div>
+  </div>
+  <div class="row">
+    <div class="input-field col s12">
+      <input type="text" name="tempo">
+      <label for="tempo">Tempo (Meses)</label>
+    </div>
+  </div>
+  <div class="row">
+    <button class="btn waves-effect waves-light" type="submit" name="action">Calcular
+      <i class="material-icons right">send</i>
+    </button>
+  </div>
+</form>
+</div>
+</div>
+
+
+<div class="card">
+<div class="card-content">
+  <div class="card-title">Juros Composto</div>
+<form class="col s12" action="../../controller/jurosComposto.controller.php" method="POST">
+  <div class="row">
+    <div class="input-field col s6">
+      <input id="capital" type="text" name="capital">
+      <label for="capital">Capital (R$)</label>
+    </div>
+    <div class="input-field col s6">
+      <input id="taxa" type="text" name="taxa" class="validate">
+      <label for="taxa">Taxa (% Mês)</label>
+    </div>
+  </div>
+  <div class="row">
+    <div class="input-field col s12">
+      <input type="text" name="tempo">
+      <label for="tempo">Tempo (Meses)</label>
+    </div>
+  </div>
+  <div class="row">
+    <button class="btn waves-effect waves-light" type="submit" name="action">Calcular
+      <i class="material-icons right">send</i>
+    </button>
+  </div>
+</form>
+</div>
+</div>
+<!-- <h5>Juros simples</h5>
 <form class="" action="../../controller/jurosSimples.controller.php" method="POST">
   capital<input type="text" name="capital">
   taxa<input type="text" name="taxa">
@@ -26,12 +69,12 @@
 
 <h5>Juros Composto</h5>
 
-<form class="" action="../../controller/jurosComposto.controller.php" method="post">
+<form class="" action="" method="post">
   capital<input type="text" name="capital">
   taxa<input type="text" name="taxa">
   tempo<input type="text" name="tempo">
   <input type="radio" name="mesAno" value="mes">Mes
   <input type="radio" name="mesAno" value="ano">Ano
   <button type="submit" name="button">Calcular</button>
-</form>
+</form> -->
 <?php require_once '../_includes/footer.php'; ?>
